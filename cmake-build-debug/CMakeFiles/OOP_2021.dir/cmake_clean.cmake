477df8587b8fc5659c06fe5c44bc1669b51c9c5f
deblur99 <deblur99@kakao.com>
2021-09-10 18:54:05 +0900

    first-commit

---
+++ cmake-build-debug/CMakeFiles/OOP_2021.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_2021.dir/main.cpp.obj"
+  "OOP_2021.exe"
+  "OOP_2021.exe.manifest"
+  "OOP_2021.pdb"
+  "libOOP_2021.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_2021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string foo = "Hello ";
+    std::string bar = "World";
+
+    std::cout << foo + bar << std::endl;
+    return 0;
+}
