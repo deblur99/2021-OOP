bb34e02f46b904745df4c6d88f1aed795c8c39f8
deblur99 <deblur99@kakao.com>
2021-09-30 16:33:13 +0900

    added-hw2

--- cmake-build-debug/CMakeFiles/OOP_2021.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/OOP_2021.dir/DependInfo.cmake
@@ -8,11 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-<<<<<<< HEAD
   "C:/Users/deblu/CLionProjects/OOP_2021/hw2.cpp" "C:/Users/deblu/CLionProjects/OOP_2021/cmake-build-debug/CMakeFiles/OOP_2021.dir/hw2.cpp.obj"
-=======
-  "C:/Users/Deblur/CLionProjects/2021-OOP/hw1.cpp" "C:/Users/Deblur/CLionProjects/2021-OOP/cmake-build-debug/CMakeFiles/OOP_2021.dir/hw1.cpp.obj"
->>>>>>> ed0c3474b5f433f411974e7adec0f0b26a924c06
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
